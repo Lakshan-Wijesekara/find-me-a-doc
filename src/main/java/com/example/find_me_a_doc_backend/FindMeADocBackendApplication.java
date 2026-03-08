c9c1f4e6f68d288d73f8cb6421d670d17d156fad
LakshanWijesekara <lakshan.wijesekara@oneflow.com>
2026-03-08 20:55:16 +0530

    Initial commit for the project

---
+++ src/main/java/com/example/find_me_a_doc_backend/FindMeADocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.find_me_a_doc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeADocBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMeADocBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/find_me_a_doc_backend/FindMeADocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.find_me_a_doc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMeADocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
