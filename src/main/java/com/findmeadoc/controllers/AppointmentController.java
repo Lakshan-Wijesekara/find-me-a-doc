4ce12d9758396667b43972cd4a30ba574c9f516c
Lakshan-Wijesekara <lwijesekara149@gmail.com>
2026-04-23 00:36:19 +0530

    * Add the doctor dashboard response * Add the update logic for the dashboard * Add endpoints for the dashboard get request of appointments for a specific doctor

--- src/main/java/com/findmeadoc/controllers/AppointmentController.java
+++ src/main/java/com/findmeadoc/controllers/AppointmentController.java
@@ -1,11 +1,9 @@
 package com.findmeadoc.controllers;
 
-import com.findmeadoc.application.dto.AppointmentDashboardResponse;
-import com.findmeadoc.application.dto.AvailableSlotResponse;
-import com.findmeadoc.application.dto.CreateAppointmentRequest;
-import com.findmeadoc.application.dto.CreateAppointmentResponse;
+import com.findmeadoc.application.dto.*;
 import com.findmeadoc.application.ports.BookAppointmentUseCase;
 import com.findmeadoc.application.ports.GetAvailableSlotsUseCase;
+import com.findmeadoc.application.ports.GetDoctorAppointmentsUseCase;
 import com.findmeadoc.application.ports.GetPatientAppointmentUseCase;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -24,15 +22,18 @@ public class AppointmentController {
     private final BookAppointmentUseCase bookAppointmentUseCase;
     private final GetPatientAppointmentUseCase getPatientAppointmentUseCase;
     private final GetAvailableSlotsUseCase getAvailableSlotsUseCase;
+    private final GetDoctorAppointmentsUseCase getDoctorAppointmentsUseCase;
 
     public AppointmentController(
             BookAppointmentUseCase bookAppointmentUseCase,
             GetPatientAppointmentUseCase getPatientAppointmentUseCase,
-            GetAvailableSlotsUseCase getAvailableSlotsUseCase
+            GetAvailableSlotsUseCase getAvailableSlotsUseCase,
+            GetDoctorAppointmentsUseCase getDoctorAppointmentsUseCase
     ) {
         this.bookAppointmentUseCase = bookAppointmentUseCase;
         this.getPatientAppointmentUseCase = getPatientAppointmentUseCase;
         this.getAvailableSlotsUseCase = getAvailableSlotsUseCase;
+        this.getDoctorAppointmentsUseCase = getDoctorAppointmentsUseCase;
     }
 
     @PostMapping
@@ -71,6 +72,20 @@ public class AppointmentController {
 
         // Return 200 OK with the list of available times
         return ResponseEntity.ok(availableSlots);
+    }
+
+    @GetMapping("/doctor")
+    public ResponseEntity<List<DoctorAppointmentDashboardResponse>> getDoctorAppointments(
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        // Extract the doctor's email from the JWT
+        String doctorEmail = userDetails.getUsername();
+
+        // Fetch the appointments via the Service
+        List<DoctorAppointmentDashboardResponse> appointments = getDoctorAppointmentsUseCase.execute(doctorEmail);
+
+        // Return a 200 OK with the list
+        return ResponseEntity.ok(appointments);
     }
 
 }
